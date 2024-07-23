365cf38212bff33ea5baecde7ad4da67ed3ba911
AEscobarAngola <abrancho1908@gmail.com>
2024-07-23 21:16:45 +0200

    update: correction on header file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,15 +6,9 @@ export const Header = () => {
     return (
         <>
             <div className='Header-root'>
-                <div>
-                    <div>logo</div>
-                </div>
-                <div></div>
-                <div id='navigation-links'>
                     <div className='center-navigation'><CSurfer content="Home" path="/" /></div>
                     <div className='center-navigation'><CSurfer content="servicios" path="/" /></div>
                     <div className='center-navigation'><CSurfer content="citas" path="/" /></div>
-                </div>
             </div>
 
         </>
