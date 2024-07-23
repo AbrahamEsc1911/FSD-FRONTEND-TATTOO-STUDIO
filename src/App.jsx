5f0347d8b2386a99dad5d8d9dc6809345648ffac
AEscobarAngola <abrancho1908@gmail.com>
2024-07-23 21:17:29 +0200

    feat: body routers created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Footer } from './components/Footer/Footer.jsx'
 import { Header } from './components/Header/Header.jsx'
+import { Body } from './views/Body/Body.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Body />
       <Footer />
     </>
   )
