a45df1b99cad3a61ca064a48d21ae6bdceef9241
AEscobarAngola <abrancho1908@gmail.com>
2024-07-23 19:54:04 +0200

    feat: footer component created

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { CSurfer } from '../Csurfer/CSurfer.jsx';
 
-export const Footer = ({name}) => {
 
-    
+export const Footer = () => {
 
-  return (
-    <>
-    </>
-  )
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div>
+                <div>
+                    <div><CSurfer path="/" content="Home" /></div>
+                    <div><CSurfer path="/" content="Books" /></div>
+                    <div><CSurfer path="/" content="Register" /></div>
+                </div>
+                <div>
+                    <p>All rigths reserved</p>
+                </div>
+            </div>
+
+
+        </>
+    )
 }
