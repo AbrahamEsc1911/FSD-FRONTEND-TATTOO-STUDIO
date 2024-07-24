23a2b38eeed62a33d17fca2ddb25507d92b9b9db
AEscobarAngola <abrancho1908@gmail.com>
2024-07-24 17:35:08 +0200

    wip: adding some conditions to show a message about email and possword required

--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -1,21 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Cinput } from '../../components/Cinput/Cinput.jsx'
+import './Register.css'
 
 export const Register = () => {
 
-    const handleChange = () => {
+  const [registerData, setregisterData] = useState(
+    {
+      name: "",
+      email: "",
+      password: ""
+    }
+  )
 
+  const [hideContent, sethideContent] = useState(false)
+  const [passwordLenght, setpasswordLenght] = useState(false)
+
+  const handleChange = (e) => {
+    setregisterData(prevState => (
+      {
+        ...prevState,
+        [e.target.name]: e.target.value
+      }
+    )) 
+  }
+  console.log(registerData)
+  const register = () => {
+    if(registerData.email.length === 0 || registerData.password.length === 0){
+      return sethideContent(true)
+    } else if (registerData.email.length > 0 && registerData.password.length > 0){
+       sethideContent(false)
     }
 
-    const register = () => {
+    if(registerData.password.length < 8 || registerData.password.length > 12) {
+      return setpasswordLenght(true)
+    } else if (registerData.password.length > 7 || registerData.password.length < 12){
+      setpasswordLenght(false)
+   }
 
-    }
+
+  }
 
   return (
     <>
-    <Cinput type="text" name="email" placeholder="Email" emitFuntion={handleChange} />
-    <Cinput type="password" name="password" placeholder="password" emitFuntion={handleChange} />
-    <Cinput type="button" name="register" value="Register" onClickFuntion={register} />
+      <h1>Registro</h1>
+      <div><Cinput type="text" name="email" placeholder="Email (Opcional)" emitFuntion={handleChange} /></div>
+      <div><Cinput type="email" name="email" placeholder="email" emitFuntion={handleChange} /></div>
+      <div><Cinput type="password" name="password" placeholder="password" emitFuntion={handleChange} /></div>
+      <p className={hideContent ? "" : "hidden-content"}>El correo y la contraseña son obligatorios</p>
+      <p className={passwordLenght ? "" : "hidden-content"}>la contraseña debe ser mayor a 8 y menor a 12</p>
+      <div><Cinput type="button" name="register" value="Register" onClickFuntion={register} /></div>
     </>
   )
 }
