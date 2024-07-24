d4d67f0ac8083f220d20e7abc255135f66d17f38
AEscobarAngola <abrancho1908@gmail.com>
2024-07-24 18:47:22 +0200

    feat: created new view of login, add path on body

--- src/views/Body/Body.jsx
+++ src/views/Body/Body.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Register } from '../Register/Register.jsx'
+import { Login } from '../Login/Login.jsx'
 
 export const Body = () => {
   return (
     <>
     <Routes>
         <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
     </Routes>
     </>
   )
---
+++ src/views/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+import { Cinput } from '../../components/Cinput/Cinput.jsx'
+
+export const Login = () => {
+  return (
+    <>
+ 
+    </>
+  )
+}
+  
