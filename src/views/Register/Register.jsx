07294282068279f591329140b362a8542fc32f6b
AEscobarAngola <abrancho1908@gmail.com>
2024-07-24 18:24:12 +0200

    update: error message showed on front

--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -17,8 +17,10 @@ export const Register = () => {
   )
 
   const [hideContent, sethideContent] = useState(false)
+
   const [passwordLenght, setpasswordLenght] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(false)
+  
+  const [errorMessage, setErrorMessage] = useState("")
 
   const handleChange = (e) => {
     setregisterData(prevState => (
@@ -31,25 +33,26 @@ export const Register = () => {
 
   const register = async () => {
 
-      if (registerData.email.length === 0 || registerData.password.length === 0) {
-        return sethideContent(true)
-      } else if (registerData.email.length > 0 && registerData.password.length > 0) {
-        sethideContent(false)
-      }
+    if (registerData.email.length === 0 || registerData.password.length === 0) {
+      return sethideContent(true)
+    } else if (registerData.email.length > 0 && registerData.password.length > 0) {
+      sethideContent(false)
+    }
 
-      if (registerData.password.length < 8 || registerData.password.length > 12) {
-        return setpasswordLenght(true)
-      } else if (registerData.password.length > 7 || registerData.password.length < 12) {
-        setpasswordLenght(false)
-      }
+    if (registerData.password.length < 8 || registerData.password.length > 12) {
+      return setpasswordLenght(true)
+    } else if (registerData.password.length > 7 || registerData.password.length < 12) {
+      setpasswordLenght(false)
+    }
 
-      const response = await registerUsers(registerData)
+    const response = await registerUsers(registerData)
 
-      if(response.success) {
-        navigate("/login")
-      } else {
-        return response.message
-      }
+
+    if (response.success) {
+      navigate("/login")
+    } else {
+      return setErrorMessage(response.message)
+    }
 
   }
 
@@ -61,7 +64,7 @@ export const Register = () => {
       <div><Cinput type="password" name="password" placeholder="password" emitFuntion={handleChange} /></div>
       <p className={hideContent ? "" : "hidden-content"}>El correo y la contraseña son obligatorios</p>
       <p className={passwordLenght ? "" : "hidden-content"}>la contraseña debe ser mayor a 8 y menor a 12</p>
-      <p className={errorMessage ? "" : "hidden-content"}>No se pudo hacer el registro</p>
+      <p >{errorMessage}</p>
       <div><Cinput type="button" name="register" value="Register" onClickFuntion={register} /></div>
     </>
   )
