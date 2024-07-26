8d8166b4b940688b00646496857929f71682c6ee
AEscobarAngola <abrancho1908@gmail.com>
2024-07-26 18:54:59 +0200

    update: some corrections, handler events adding corretly data on array

--- src/views/NewApp/NewApp.jsx
+++ src/views/NewApp/NewApp.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Cinput } from '../../components/Cinput/Cinput'
 import { useNavigate } from 'react-router-dom'
+import { Cselect } from '../../components/Cselect/Cselect'
+import { getAllArtists } from '../../services/apiCalls'
+import { getAllServices } from '../../services/services.services'
 
 export const NewApp = () => {
 
@@ -10,7 +13,6 @@ export const NewApp = () => {
 
     passport ? id = passport.tokenData.id : navigate("/login")
 
-    console.log(id)
     const [newAppoinment, setNewAppointment] = useState(
         {
             users_id: id,
@@ -20,22 +22,41 @@ export const NewApp = () => {
         }
     )
 
+    const [services, setServices] = useState([])
+    const [artists, setArtists] = useState([])
+
+    useEffect(() => {
+        const servicesAndArtist = async () => {
+            const services = await getAllServices()
+            const artists = await getAllArtists()
+            console.log(artists.success)
+            if (services.success && services.success) {
+                setServices(services.data)
+                setArtists(artists.data)
+            }
+        };
+        servicesAndArtist()
+    }, [])
+
+
+    console.log(artists)
     const handleEvents = (e) => {
         setNewAppointment({
             ...newAppoinment,
             [e.target.name]: e.target.value,
         });
     };
-    console.log(newAppoinment)
 
     const today = new Date().toISOString().split('T')[0];
-
+    console.log(newAppoinment)
     return (
         <>
             <h1>Nueva Cita</h1>
-        
-            < Cinput type="date" min={today} value={newAppoinment.due_date} name="due_date" emitFuntion={handleEvents} />
 
+
+            < Cinput type="date" min={today} value={newAppoinment.due_date} name="due_date" emitFuntion={handleEvents} />
+            < Cselect name={"services_id"} category="Servicios" options={services} emitFunction={handleEvents}/>
+            < Cselect name={"artists_id"} category="Artistas" options={artists} emitFunction={handleEvents}/>
         </>
     )
 }
