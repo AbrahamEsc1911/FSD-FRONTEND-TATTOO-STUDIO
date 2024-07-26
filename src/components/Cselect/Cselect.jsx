4ddbd2c51612345665f8b71ef015345b22ff4a7e
AEscobarAngola <abrancho1908@gmail.com>
2024-07-26 18:54:20 +0200

    fix: custom select fixed, name adde to allow to use a name as a key for the handlerEvents

--- src/components/Cselect/Cselect.jsx
+++ src/components/Cselect/Cselect.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 
-export const Cselect = ({ category, options, emitFunction }) => {
+export const Cselect = ({ category, options, emitFunction, name }) => {
     return (
         <>
-            <select defaultValue="" onChange={emitFunction}>
-                <option disabled hidden value="">
-                    {category}
-                </option>
+            <select defaultValue="" name={name} onChange={emitFunction}>
+                <option disabled hidden value="">{category}</option>
                 {options.map((option) => {
-                    return <option value={option.id} key={option.id}>{option.serviceName}</option>
+                    return <option value={option.id} key={option.id}>{option.name}</option>
                 })}
             </select>
         </>
-    )
-}
+    );
+};
