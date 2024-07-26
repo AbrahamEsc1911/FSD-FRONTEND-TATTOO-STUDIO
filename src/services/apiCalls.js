2401e470319c9b30645e24406a37786d6c307ed6
AEscobarAngola <abrancho1908@gmail.com>
2024-07-26 16:25:53 +0200

    feat: get all apoinments by user, view created

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -118,3 +118,16 @@ export const getAllArtists = async () => {
 
     return await response.json()
 }
+
+export const getAllAppoinmentsByI = async (token) => {
+    console.log(token)
+    const response = await fetch(`${URL}/appointments`, {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": `Beare ${token}`
+        }
+    })
+
+    return await response.json()
+}
--- src/views/Appointments/Appointments.jsx
+++ src/views/Appointments/Appointments.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { getAllAppoinmentsByI } from '../../services/apiCalls'
 
 export const Appointments = () => {
-  return (
-    <div>Appointments</div>
-  )
+
+    const passport = JSON.parse(localStorage.getItem("passport"))
+    let token = null
+    if (passport) { token = passport.token }
+    const navegate = useNavigate()
+    const [appointments, setAppointments] = useState([])
+    const [error, setError] = useState("")
+
+    useEffect(() => {
+
+        if (!passport) {
+            navegate("/login")
+        }
+
+        const allAppoinments = async () => {
+            const response = await getAllAppoinmentsByI (token)
+            console.log(response)
+            if(response.success){
+                setAppointments(response.data)
+            } else (
+                setError(response.message)
+            )
+        };
+        allAppoinments()
+
+    }, [])
+
+    return (
+        <div>Appointments
+            <div>
+            {appointments.length && appointments.map((app) => {
+                    return (
+                        <div key={app.id}>
+                            <h2>codigo de cita No.{app.id}</h2>
+                            <h4>Servicio</h4>
+                            <div>{app.service.name}</div>
+                            <h4>Artista</h4>
+                            <div>{app.artist.name}</div>
+                            <h4>Fecha</h4>
+                            <div>{app.due_date}</div>
+                        </div>
+                    )
+                })}
+            </div>
+            <div>{error}</div>
+        </div>
+    )
 }
